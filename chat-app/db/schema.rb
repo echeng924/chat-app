147554ac3cc95767cd266c13fdd3f0826aaa0105
Elizabeth Cheng <echeng924@gmail.com>
2019-04-10 16:18:10 -0400

    Swap out sqlite for postres in gemfile, add models for chat, message, and user

--- chat-app/db/schema.rb
+++ chat-app/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20190410195555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chats", force: :cascade do |t|
     t.string "name"
   end
