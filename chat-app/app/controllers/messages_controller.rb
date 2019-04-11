f605215390ed179e2921bfc94dd50b9e24745a2a
Elizabeth Cheng <echeng924@gmail.com>
2019-04-10 23:51:03 -0400

    Update messages and users controller with create, log_in, and sign_up endpoints

--- chat-app/app/controllers/messages_controller.rb
+++ chat-app/app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
 class MessagesController < ApplicationController
+    def create
+        @message = Message.create(
+            user_id: params[:user_id],
+            chat_id: params[:chat_id],
+            text: params[:text] 
+        )
+
+        if @message.save
+            render json: {
+                messageText: @message.text
+            }
+        end
+    end
 end
--- chat-app/app/controllers/users_controller.rb
+++ chat-app/app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
     def sign_up
-        
+    end
+
+    def create
+        user_exists = User.where(name: params[:username].downcase).present?
+        if user_exists
+            render json: {
+                error: 'This user name has already been taken'
+            }
+            return
+        end
+
+        @user = User.create(name: params[:username].downcase, password: params[:password])
+
+        if @user.save
+            render json: {
+                res: true,
+                user: @user
+            }
+        end
+    end
+
+    def log_in
+        @user = User.where(name: params[:username].downcase, password: params[:password])
+
+        if @user.present?
+            render json: {
+                user: @user.first
+            }
+        else
+            render json: {
+                error: 'Your account does not exist! Please sign up.'
+            }
+        end
     end
 end
